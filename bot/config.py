fac16cbf967d5e3037eebf2e2d10e7d3aef9f86f
shodliktest <shodlikfire@gmail.com>
2026-01-17 13:37:38 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+# Kelajakda Firebase yoki boshqa sozlamalar ham shu yerga qo'shiladi
 
